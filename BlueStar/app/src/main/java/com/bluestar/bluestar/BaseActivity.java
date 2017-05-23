92004920a1ae724d5ae323bf3ce5bd618f50e3ea
manikantha1 <m8008040782@gmail.com>
2017-05-23 21:01:39 +0530

    base

--- BlueStar/app/src/main/java/com/bluestar/bluestar/BaseActivity.java
+++ BlueStar/app/src/main/java/com/bluestar/bluestar/BaseActivity.java
@@ -70,7 +70,7 @@ public class BaseActivity extends AppCompatActivity
                             .setAction("Action", null).show();
                 }
             });
-
+// manikantha is doing changes
             DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
             ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                     this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
